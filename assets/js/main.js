237111e643dace558d3291b40ca83a9a4e9187f8
Jao <ricardojoao412@gmail.com>
2026-01-21 17:53:05 -0400

    adding dinamic html list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,34 @@
 const offset=0
 const limit=10
 const url=`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonToLi(pokemon, number) {
+    return ` <li class="pokemon"> 
+                <span class="number">#00${number}</span>
+                <span class="name">${pokemon.name}</span> 
+                <div class="detail">
+                    <ol class="type">
+                        <li class="type">Fire</li>
+                        <li class="type">nosé</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png" 
+                    alt="${pokemon.name}">
+                </div>
+            </li>`
+}
+const pokemonList = document.getElementById('pokemonList')
+fetch(url)
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
-const x=10 +5
-console.log(x)
+    .then((response) => response.json())
+    .then((data) => data.results)
+    .then((pokemons) => {
+        for( let i=0; i<pokemons.length; i++){
+            const pokemonName = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemonName, i+2)
+        }
+
+    })
+    .catch((error) => console.log(error))
+
+
+  
+
