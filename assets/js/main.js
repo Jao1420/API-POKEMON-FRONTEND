524ef65852de490c45f95a8aa7bf8548c389fabc
Jao <ricardojoao412@gmail.com>
2026-01-28 15:17:35 -0400

    adicionando nova forma de converter uma lista html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,29 @@
-const offset=0
-const limit=10
-const url=`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-function convertPokemonToLi(pokemon, number) {
+function convertPokemonToLiTypes(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+function convertPokemonToLi(pokemon) {
     return ` <li class="pokemon"> 
-                <span class="number">#00${number}</span>
+                <span class="number">#00${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span> 
                 <div class="detail">
                     <ol class="type">
-                        <li class="type">Fire</li>
-                        <li class="type">nosé</li>
+                        ${convertPokemonToLiTypes(pokemon.types).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png" 
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" 
                     alt="${pokemon.name}">
                 </div>
             </li>`
 }
 const pokemonList = document.getElementById('pokemonList')
-fetch(url)
+    
+pokeApi.getPokemon().then((pokemons) => {
+    pokemonList.innerHTML+= pokemons.map(convertPokemonToLi).join('')
+})
 
-    .then((response) => response.json())
-    .then((data) => data.results)
-    .then((pokemons) => {
-        for( let i=0; i<pokemons.length; i++){
-            const pokemonName = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemonName, i+2)
-        }
 
-    })
-    .catch((error) => console.log(error))
+
+  
 
 
   
---
+++ assets/js/poke-api.js
@@ -0,0 +1,21 @@
+
+const pokeApi={}
+
+
+pokeApi.getPokemonDetail=(pokemon)=>{
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
+pokeApi.getPokemon=(offset = 0, limit = 10)=>{
+
+const url=`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+        .catch((error) => console.log(error))
+}       
+
